05b7b741a643786c552a4f8769d99a2f04d63efa
Chase <chase@csh.rit.edu>
2018-05-09 16:57:37 -0400

    Added another constructor on match

--- src/main/kotlin/edu/csh/chase/aggregations/stages/Match.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/Match.kt
@@ -1,11 +1,11 @@
 package edu.csh.chase.aggregations.stages
 
 import edu.csh.chase.aggregations.doc
-import edu.csh.chase.aggregations.plusAssign
 import org.bson.Document
 
 class Match(val document: Document) : Stage() {
 
     constructor(vararg pairs: Pair<String, Any?>) : this(doc(*pairs))
 
+    constructor(map: Map<String, Any?>) : this(Document(map))
 }
