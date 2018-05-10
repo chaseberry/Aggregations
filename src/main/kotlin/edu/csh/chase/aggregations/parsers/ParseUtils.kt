65bc134826f0b5beb74549ba8f81ee6143401f1e
Chase <chase@csh.rit.edu>
2018-05-10 10:58:10 -0400

    Work on parsing. Still WIP

--- src/main/kotlin/edu/csh/chase/aggregations/parsers/ParseUtils.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/parsers/ParseUtils.kt
@@ -5,6 +5,11 @@ import kotlin.reflect.KClass
 
 fun <K, V> Map<K, V>.nonNull(key: K) = get(key) ?: throw ValueException()
 
+
+fun <K, V> Map<K, V>.boolean(key: K): Boolean = nonNull(key) assertType Boolean::class
+fun <K, V> Map<K, V>.opBoolean(key: K): Boolean? = get(key)?.let { it assertType Boolean::class }
+
+
 fun <K, V> Map<K, V>.string(key: K): String = nonNull(key) assertType String::class
 fun <K, V> Map<K, V>.opString(key: K): String? = get(key)?.let { it assertType String::class }
 
--- src/main/kotlin/edu/csh/chase/aggregations/parsers/StageParser.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/parsers/StageParser.kt
@@ -1,8 +1,8 @@
 package edu.csh.chase.aggregations.parsers
 
 import edu.csh.chase.aggregations.exceptions.StageParsingException
-import edu.csh.chase.aggregations.stages.Match
-import edu.csh.chase.aggregations.stages.Stage
+import edu.csh.chase.aggregations.exceptions.ValueException
+import edu.csh.chase.aggregations.stages.*
 import org.bson.Document
 
 class StageParser(val input: Map<String, Any?>) {
@@ -21,17 +21,77 @@ class StageParser(val input: Map<String, Any?>) {
     }
 
     private fun parseMatchStage(): Match {
-        val doc = input["\$match"]
-
-        val map: Map<String, Any?> = when (doc) {
-            null -> throw error("Value of a Match stage cannot be null")
-            is Document -> doc
-            is Map<*, *> -> doc.mapKeys { it.key.toString() }
-            else -> throw error("Value of a match stage must be an Object")
-        }
+        val map = getMap("\$match")
 
         //TODO extra validation on stuff
         return Match(map)
+    }
+
+    private fun parseProjectStage(): Project {
+        val map = getMap("\$project")
+
+        //TODO extra validation on stuff
+        return Project(map)
+    }
+
+    private fun parseUnwind(): Unwind {
+        val map = getMap("\$unwind")
+
+        return try {
+            Unwind(
+                path = map.string("path"),
+                includeArrayIndex = map.opString("includeArrayIndex"),
+                preserveNullAndEmptyArrays = map.opBoolean("preserveNullAndEmptyArrays") ?: false
+            )
+        } catch (e: ValueException) {
+            throw error()
+        }
+    }
+
+    private fun parseLookupStage(): Stage {
+        val map = getMap("\$lookup")
+
+        return if ("pipeline" in map) {
+            parsePipelineLookup(map)
+        } else {
+            parseBasicLookup(map)
+        }
+    }
+
+    private fun parsePipelineLookup(map: Map<String, Any?>): PipelineLookup {
+        return try {
+            PipelineLookup(
+                let =,
+                pipeline =,
+                `as` = map.string("as")
+            )
+        } catch (e: ValueException) {
+            throw error()
+        }
+    }
+
+    private fun parseBasicLookup(map: Map<String, Any?>): BasicLookup {
+        return try {
+            BasicLookup(
+                from = map.string("from"),
+                localField = map.string("localField"),
+                foreignField = map.string("foreignField"),
+                `as` = map.string("as")
+            )
+        } catch (e: ValueException) {
+            throw error()
+        }
+    }
+
+    private fun getMap(stage: String): Map<String, Any?> {
+        val doc = input[stage]
+
+        return when (doc) {
+            null -> throw error("Value of a stage stage cannot be null")
+            is Document -> doc
+            is Map<*, *> -> doc.mapKeys { it.key.toString() }
+            else -> throw error("Value of a stage stage must be an Object")
+        }
     }
 
     private fun error(msg: String): StageParsingException {
