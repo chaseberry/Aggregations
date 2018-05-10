4e2479d376845d0ab92e84428f5dace1424698f7
Chase <chase@csh.rit.edu>
2018-05-10 13:06:16 -0400

    Fixed missing :Stage() on several stages

--- src/main/kotlin/edu/csh/chase/aggregations/stages/Project.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/Project.kt
@@ -3,7 +3,7 @@ package edu.csh.chase.aggregations.stages
 import edu.csh.chase.aggregations.doc
 import org.bson.Document
 
-class Project(val document: Document) {
+class Project(val document: Document) : Stage() {
 
     constructor(vararg pairs: Pair<String, Any?>) : this(doc(*pairs))
 
--- src/main/kotlin/edu/csh/chase/aggregations/stages/Sort.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/Sort.kt
@@ -3,7 +3,7 @@ package edu.csh.chase.aggregations.stages
 import edu.csh.chase.aggregations.doc
 import org.bson.Document
 
-class Sort(val document: Document) {
+class Sort(val document: Document)  : Stage(){
 
     constructor(vararg pairs: Pair<String, Int>) : this(doc(*pairs))
 
