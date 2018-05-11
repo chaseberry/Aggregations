355fc30091fdcda2d34d6c3a2fc04380a2ec55c8
Chase <chase@csh.rit.edu>
2018-05-11 16:12:25 -0400

    Added testing for ints and boolean parsing

--- src/test/kotlin/edu/csh/chase/aggregations/parsing/ValueParsingTest.kt
+++ src/test/kotlin/edu/csh/chase/aggregations/parsing/ValueParsingTest.kt
@@ -7,9 +7,25 @@ import org.junit.jupiter.api.Test
 
 class ValueParsingTest {
 
+    private fun String.parse(): Any? = Parser(this).parse()
+
     @Test
-    fun parseNull(){
-        assertEquals(null, Parser("null").parse())
+    fun parseNull() {
+        assertEquals(null, "null".parse())
+    }
+
+    @Test
+    fun parseBooleans() {
+        assertEquals(true, "true".parse())
+        assertEquals(false, "false".parse())
+    }
+
+    @Test
+    fun parseInt() {
+        assertEquals(0, "0".parse())
+        assertEquals(14, "14".parse())
+        assertEquals(173, "+173".parse())
+        assertEquals(-12, "-12".parse())
     }
 
 }
