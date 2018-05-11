ade0bb154dddb03326312826c15ffa6e449b719a
Chase <chase@csh.rit.edu>
2018-05-11 16:12:50 -0400

    Removed collection from pipeline

--- src/main/kotlin/edu/csh/chase/aggregations/Pipeline.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/Pipeline.kt
@@ -3,5 +3,5 @@ package edu.csh.chase.aggregations
 import edu.csh.chase.aggregations.stages.Stage
 
 //TODO options
-class Pipeline(val collection: String, val stages: List<Stage>) {
+class Pipeline(val stages: List<Stage>) {
 }
