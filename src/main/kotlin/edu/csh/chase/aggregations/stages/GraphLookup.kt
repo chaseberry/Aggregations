d297a5079e5797336ec9d0a8eb71c7089452a38d
Chase <chase@csh.rit.edu>
2018-05-31 08:51:57 -0400

    Added missing as field

--- src/main/kotlin/edu/csh/chase/aggregations/stages/GraphLookup.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/GraphLookup.kt
@@ -4,5 +4,5 @@ import org.bson.Document
 
 class GraphLookup(val from: String, val startWith: Any?, val connectToField: String,
                   val connectFromField: String, val maxDepth: Int?, val depthField: String?,
-                  val restrictSearchWithMatch: Document?) : Stage() {
+                  val restrictSearchWithMatch: Document?, val `as`: String) : Stage() {
 }
