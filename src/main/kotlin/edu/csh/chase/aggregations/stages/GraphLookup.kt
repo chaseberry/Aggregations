3a5b1e0a781646cb89bedfb4784938116271b90e
Chase <chase@csh.rit.edu>
2018-12-07 10:17:06 -0500

    Added render for graphLookup

--- src/main/kotlin/edu/csh/chase/aggregations/stages/GraphLookup.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/GraphLookup.kt
@@ -1,5 +1,7 @@
 package edu.csh.chase.aggregations.stages
 
+import edu.csh.chase.aggregations.AggregationsStringBuilder
+import edu.csh.chase.aggregations.RenderSettings
 import org.bson.Document
 
 class GraphLookup(val from: String,
@@ -10,4 +12,25 @@ class GraphLookup(val from: String,
                   val depthField: String?,
                   val restrictSearchWithMatch: Document?,
                   val `as`: String) : Stage("\$graphLookup") {
+
+    override fun renderSelf(depth: Int, settings: RenderSettings): String {
+        val s = AggregationsStringBuilder(settings)
+
+        s.writeMap(
+            map = mapOf(
+                "from" to from,
+                "startWith" to startWith,
+                "connectToField" to connectToField,
+                "connectFromField" to connectFromField,
+                "maxDepth" to maxDepth,
+                "depthField" to depthField,
+                "restrictSearchWithMatch" to restrictSearchWithMatch,
+                "as" to `as`
+            ),
+            depth = depth
+        )
+
+        return s.toString()
+    }
+
 }
