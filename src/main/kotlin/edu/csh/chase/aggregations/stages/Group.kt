4c4fb8799f4d71fa9f73a1e033a3bf748d9d412e
Chase <chase@csh.rit.edu>
2018-12-07 10:58:18 -0500

    Added render for group and lookup

--- src/main/kotlin/edu/csh/chase/aggregations/stages/Group.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/Group.kt
@@ -1,7 +1,26 @@
 package edu.csh.chase.aggregations.stages
 
+import edu.csh.chase.aggregations.AggregationsStringBuilder
+import edu.csh.chase.aggregations.RenderSettings
+import edu.csh.chase.aggregations.exceptions.ValueException
+import edu.csh.chase.aggregations.utils.doc
 import org.bson.Document
 
 class Group(val _id: Any?, val fields: Document) : Stage("\$group") {
+
+    constructor(vararg fields: Pair<String, Any?>) : this(
+        _id = fields.find { it.first == "_id" } ?: throw ValueException("_id", "_id field", ""),
+        fields = doc(*(fields)).apply { remove("_id") }
+    )
+
+    override fun renderSelf(depth: Int, settings: RenderSettings): String {
+        val s = AggregationsStringBuilder(settings)
+
+        fields["_id"] = _id
+
+        s.writeMap(fields, depth)
+
+        return s.toString()
+    }
 
 }
--- src/main/kotlin/edu/csh/chase/aggregations/stages/Lookup.kt
+++ src/main/kotlin/edu/csh/chase/aggregations/stages/Lookup.kt
@@ -1,4 +1,27 @@
 package edu.csh.chase.aggregations.stages
 
-class Lookup(val from: String, val localField: String, val foreignField: String, val `as`: String) : Stage("\$lookup") {
+import edu.csh.chase.aggregations.AggregationsStringBuilder
+import edu.csh.chase.aggregations.RenderSettings
+
+class Lookup(val from: String,
+             val localField: String,
+             val foreignField: String,
+             val `as`: String) : Stage("\$lookup") {
+
+    override fun renderSelf(depth: Int, settings: RenderSettings): String {
+        val s = AggregationsStringBuilder(settings)
+
+        s.writeMap(
+            depth = depth,
+            map = mapOf(
+                "from" to from,
+                "localField" to localField,
+                "foreignField" to foreignField,
+                "as" to `as`
+            )
+        )
+
+        return s.toString()
+    }
+
 }
